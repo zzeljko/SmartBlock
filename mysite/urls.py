f8400f7a992ce2c56998ebec3349d34e18f6d0e7
Bogdan Savescu <bogdan9517@gmail.com>
2017-11-18 18:26:19 +0200

    login

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
+    url(r'^login/$', auth_views.login, name='login'),
 ]
