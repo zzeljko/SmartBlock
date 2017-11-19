985efd6088a9655c938a1a63f2ee69cde70e6648
Zeljko <zurzic_doru@yahoo.com>
2017-11-19 17:38:15 +0200

    tabs page

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 # def post_list(request):
 #     return render(request, 'blog/post_list.html')
-
+@login_required(login_url="/login/")
 def home_page(request):
+	if not request.user.is_authenticated():
+		return HttpResponseRedirect('/')
 	return render(request, 'blog/home.html')
