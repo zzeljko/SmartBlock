375f2b10068a253563a0ace82bcf5d49f5a87501
Zeljko <zurzic_doru@yahoo.com>
2018-01-14 21:36:38 +0200

    added common bills

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,8 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.admin import UserAdmin
 from django import forms
 from .models import PollQuestion, PollChoice
-from .models import Key, User, ImportantDate, OtherImportantContact
+from .models import Key, User, ImportantDate, OtherImportantContact, CommonBill
+from .widgets import MonthYearWidget
 
 class UserCreationFormExtended(UserCreationForm):
     """A form for creating new users. Includes all the required
@@ -31,28 +32,27 @@ class UserCreationFormExtended(UserCreationForm):
         	user.save()
         return user
 
-# class UserChangeFormExtended(UserChangeForm):
+class UserChangeFormExtended(UserChangeForm):
 
-#     def __init__(self, *args, **kwargs):
-#         super(UserChangeFormExtended, self).__init__(*args, **kwargs)
-#         self.fields['payed_amount'].required = True
-
+    def __init__(self, *args, **kwargs):
+        super(UserChangeFormExtended, self).__init__(*args, **kwargs)
 
 class CustomUserAdmin(UserAdmin):
 	model = User
 	list_display = UserAdmin.list_display + ('phone_number', 'is_president', 'is_in_executive_committee', 'is_administrator',)
 	add_form = UserCreationFormExtended
-	# form = UserChangeFormExtended
+	form = UserChangeFormExtended
 	add_fieldsets = (
     	(None, {
         	'classes': ('wide',),
         	'fields': ('username', 'password1', 'password2', 'no_of_persons', 'surface_factor', 'payed_amount', 'phone_number', 'email', 'is_president', 'is_in_executive_committee', 'is_administrator', )
     	}),
-	)
-    fieldsets = (
-        (None, {
-            }),
-    )
+    	)
+        fieldsets = (
+                    (None, {
+                        'fields': ('no_of_persons', 'surface_factor', 'payed_amount', 'phone_number', 'is_president', 'is_in_executive_committee', 'is_administrator', )
+                    }),
+                ) + UserAdmin.fieldsets
 
 class KeyAdmin(admin.ModelAdmin):
     fields = ('name','owner')
@@ -73,3 +73,4 @@ admin.site.register(Key, KeyAdmin)
 admin.site.register(PollQuestion, PollQuestionAdmin)
 admin.site.register(ImportantDate)
 admin.site.register(OtherImportantContact)
+admin.site.register(CommonBill)
